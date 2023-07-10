fb2c20682d1c66ba9998c3e2a4007552c57968d0
Ilia Lebedev <bacherko@gmail.com>
2023-07-10 10:49:57 +0200

    0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Flat World Game
+
+## Demonstration
+
+- v0.1.0
+
+![v0.1.0](./screenshots/v0.1.0.png)
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/flat-world-game/',
 
   // https://dev.to/tilly/aliasing-in-vite-w-typescript-1lfo
   // https://stackoverflow.com/questions/66043612/vue3-vite-project-alias-src-to-not-working
